fe1c617294843d84b3b90dc84646f1c2c863b89d
chrisbarber7 <chris@chrisbarber.co.uk>
2016-09-20 20:35:32 +0100

    Add project files.

---
+++ FitnessViewer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitnessViewer.Startup))]
+namespace FitnessViewer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
