bc90786a47d8899681c070cca30bf11213614956
chrisbarber7 <chris@chrisbarber.co.uk>
2016-09-25 20:27:05 +0100

    Methods to convert distances

---
+++ FitnessViewer.Infrastructure/Helpers/MetreDistance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessViewer.Infrastructure.Helpers
+{
+    /// <summary>
+    /// Convert from metre distances (used by Strava) to user preferred units.
+    /// </summary>
+    public static class MetreDistance
+    {
+        private const double METRE_TO_MILE = 0.00062137119;
+        private const double METER_TO_KM = 0.001;
+        private const double METER_TO_FEET = 3.2808399;
+        private const int PRECISION = 2;
+
+        /// <summary>
+        /// Convert Metre distance to miles
+        /// </summary>
+        /// <param name="metres">metre distance to convert</param>
+        /// <returns></returns>
+        public static double ToMiles(double metres)
+        {
+            return Math.Round(metres * METRE_TO_MILE, PRECISION);
+        }
+
+        /// <summary>
+        /// Convert Metre distance to KM
+        /// </summary>
+        /// <param name="metres">metre distance to convert</param>
+        /// <returns></returns>
+        public static double ToKM(double metres)
+        {
+            return Math.Round(metres * METER_TO_KM, PRECISION);
+        }
+
+        /// <summary>
+        /// Convert Metre distance to feet
+        /// </summary>
+        /// <param name="metres">metre distance to convert</param>
+        /// <returns></returns>
+        public static double ToFeet(double metres)
+        {
+            return Math.Round(metres * METER_TO_FEET, PRECISION);
+        }
+    }
+}
---
+++ FitnessViewer.Test/MetreDistanceConversionTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FitnessViewer.Infrastructure.Helpers;
+
+namespace FitnessViewer.Test
+{
+    [TestClass]
+    public class MetreDistanceConversionTest
+    {
+        [TestMethod]
+        public void MetresToMiles()
+        {
+            Assert.AreEqual(0.06, MetreDistance.ToMiles(100));
+            Assert.AreEqual(0.16, MetreDistance.ToMiles(250));
+            Assert.AreEqual(0.31, MetreDistance.ToMiles(500));
+            Assert.AreEqual(0.62, MetreDistance.ToMiles(1000));
+            Assert.AreEqual(3.11, MetreDistance.ToMiles(5000));
+            Assert.AreEqual(6.21, MetreDistance.ToMiles(10000));
+        }
+
+        [TestMethod]
+        public void MetersToFeet()
+        {
+            Assert.AreEqual(32.81, MetreDistance.ToFeet(10));
+            Assert.AreEqual(164.04, MetreDistance.ToFeet(50));
+            Assert.AreEqual(328.08, MetreDistance.ToFeet(100));
+            Assert.AreEqual(2460.63, MetreDistance.ToFeet(750));
+            Assert.AreEqual(8202.1, MetreDistance.ToFeet(2500));
+        }
+
+        [TestMethod]
+        public void MetersToKM()
+        {
+            Assert.AreEqual(10, MetreDistance.ToKM(10000));
+            Assert.AreEqual(5, MetreDistance.ToKM(5000));
+        }
+    }
+}
