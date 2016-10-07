377cfea2cf972dd318ecd1fd039f62775d66e30a
chrisbarber7 <chris@chrisbarber.co.uk>
2016-10-07 11:22:11 +0100

    Highlight selected segment on the  map

--- FitnessViewer/Scripts/app/Controllers/ViewActivityController.js
+++ FitnessViewer/Scripts/app/Controllers/ViewActivityController.js
@@ -1,35 +1,33 @@
 ﻿$(document).ready(function () {
+
+    var selectedPolyline;
+
+    // user clicks on any of the options in the lap info panel.  We'll load the summary info for that section and highlight the section on the map.
     $('ul.laps li').click(function (e) {
         var startIndex = $(this).attr("data-start-index");
         var endIndex = $(this).attr("data-end-index");
         var activityId = document.getElementById('activityId').value;
         $("#activitySummaryInformation").load("/Activity/GetSummaryInformation?activityId=" + activityId + "&startIndex=" + startIndex + "&endIndex=" + endIndex);
+
+        // if previous selection exists then removeit
+        if (selectedPolyline!=undefined)
+            mymap.removeLayer(selectedPolyline);
+
+        // get full route coords and just keep the section we are interested in.
+        var selectedCoors = getCoords(activityId).slice(startIndex, endIndex);
+
+        selectedPolyline = L.polyline(selectedCoors, { color : 'blue' }).addTo(mymap);
+
+        // un-comment to zoom in on selected polyline.  Undecided if I like it or not as hard to see where on route you are!
+        //mymap.fitBounds(selectedPolyline.getBounds());
     });
 
-    var mymap = L.map('mapid');//.setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid');
 	var activityId = document.getElementById('activityId').value;
-	var routeCoords = (function () {
-	    var routeCoords = null;
-	    $.ajax({
-	        async: false,
-	        global: false,
-	        dataType: "json",
-	        type: "POST",
-	        url: "/api/Activity/GetMapCoords/" + activityId,
-	        success: function (data) {
-	            routeCoords = data;
-	        },
-	        error: function () {
-	            alert("Error getting coords data!");
-	        }
-	    });
-	    return routeCoords;
-	})();
 
+	var routeCoords = getCoords(activityId);
 	var polyline = L.polyline(routeCoords, { color: 'red' }).addTo(mymap);
-
 	mymap.fitBounds(polyline.getBounds());
-
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=sk.eyJ1IjoiY2hyaXNiYXJiZXI3IiwiYSI6ImNpdHlxcDdnOTAwNGUzbm9hMDNueDBla2IifQ.uCc724sMqgSk316I0XuPlA', {
 	    maxZoom: 18,
@@ -37,10 +35,27 @@
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 			    id: 'mapbox.streets'
-	}).addTo(mymap);
+    }).addTo(mymap);
+
+    function getCoords(activityId) {
+    var routeCoords = null;
+    $.ajax({
+        async: false,
+        global: false,
+        dataType: "json",
+        type: "POST",
+        url: "/api/Activity/GetMapCoords/" + activityId,
+        success: function (data) {
+            routeCoords = data;
+        },
+        error: function () {
+            alert("Error getting coords data!");
+        }
+    });
+    return routeCoords;
+};
 
 });
-
 
 //function initActivityMap() {
 //    var map = new google.maps.Map(document.getElementById('map'), {
