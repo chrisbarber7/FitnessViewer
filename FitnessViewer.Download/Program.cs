7a4f5840fd5821f3611944e287ec92441a150880
chrisbarber7 <chris@chrisbarber.co.uk>
2016-10-12 15:01:28 +0100

    Add a loop so that it'll re-check queue after each pass and process again if items have been added.

--- FitnessViewer.Download/Program.cs
+++ FitnessViewer.Download/Program.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using static FitnessViewer.Infrastructure.Configuration.AutoMapperConfig;
+using FitnessViewer.Infrastructure.Data;
 
 namespace FitnessViewer.Download
 {
@@ -16,51 +17,81 @@ namespace FitnessViewer.Download
         static void Main(string[] args)
         {
             AutoMapperConfig();
-            
-       Infrastructure.Data.UnitOfWork _unitOfWork = new Infrastructure.Data.UnitOfWork();
+
+            UnitOfWork _unitOfWork = new Infrastructure.Data.UnitOfWork();
 
             // un-comment to force recalculation of peak information from stream table.
-         //     StreamHelper.RecalculateAllActivities();
-         //    StreamHelper.RecalculateSingleActivity(95887633);
+            //     StreamHelper.RecalculateAllActivities();
+            //    StreamHelper.RecalculateSingleActivity(95887633);
 
-            var jobs = _unitOfWork.Queue.GetQueue();
-
-            foreach (DownloadQueue job in jobs)
+            while (true)
             {
-                try
-                {
-                    if (job.HasError != null)
-                        if (job.HasError.Value)
-                            continue;
+                var jobs = _unitOfWork.Queue.GetQueue();
 
-                    if (job.ActivityId == null)
-                    {
-                        StravaActivityScan s = new StravaActivityScan(job.UserId);
-                        s.AddActivitesForAthlete();
-                        _unitOfWork.Queue.RemoveQueueItem(job.Id);
-                        _unitOfWork.Complete();
-                    }
-                    else
-                    {
-                        StravaActivityDownload s = new StravaActivityDownload(job.UserId);
-                        s.ActivityDetailsDownload(job.ActivityId.Value);
-                        _unitOfWork.Queue.RemoveQueueItem(job.Id);
-                        _unitOfWork.Complete();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    _unitOfWork.Queue.QueueItemMarkHasError(job.Id);
-                    _unitOfWork.Complete();
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                }
-                finally
-                {
+                if (jobs.Count() == 0)
+                    break;
 
-                }
+                ProrcessJobs(_unitOfWork, jobs);
             }
         }
 
+        /// <summary>
+        /// Process jobs in queue
+        /// </summary>
+        /// <param name="uow">UnitOfWork</param>
+        /// <param name="jobs">list of jobs to process</param>
+        private static void ProrcessJobs(UnitOfWork uow, IEnumerable<DownloadQueue> jobs)
+        {
+            foreach (DownloadQueue job in jobs)
+                ProcessJob(uow, job);
+        }
+
+        /// <summary>
+        /// Process individual job.
+        /// </summary>
+        /// <param name="uow"></param>
+        /// <param name="job"></param>
+        private static void ProcessJob(UnitOfWork uow, DownloadQueue job)
+        {
+            try
+            {
+                // if previously processed and had an error skip it
+                if (job.HasError != null)
+                    if (job.HasError.Value)
+                        return;
+
+                if (job.ActivityId != null)
+                {
+                    StravaActivityDownload s = new StravaActivityDownload(job.UserId);
+                    s.ActivityDetailsDownload(job.ActivityId.Value);
+                    uow.Queue.RemoveQueueItem(job.Id);
+                    uow.Complete();
+                }
+                else
+                { 
+                    // if job isn't for an individual activity then it must be a request to search for new activities.
+                    StravaActivityScan s = new StravaActivityScan(job.UserId);
+                    s.AddActivitesForAthlete();
+                    uow.Queue.RemoveQueueItem(job.Id);
+                    uow.Complete();
+                }
+           
+            }
+            catch (Exception ex)
+            {
+                uow.Queue.QueueItemMarkHasError(job.Id);
+                uow.Complete();
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Setup automapper
+        /// </summary>
         private static void AutoMapperConfig()
         {
             Mapper.Initialize(cfg =>
