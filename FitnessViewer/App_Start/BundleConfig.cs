7a15bae415a6de3a23138fb935c3540a4a4bccb8
chrisbarber7 <chris@chrisbarber.co.uk>
2016-10-16 22:52:03 +0100

    Notification.  Bootstrap popover added.  Showing list of activity types - needs changing to show actual action (eg activity which was downloaded rather than just 'Activity Downloaded'

--- FitnessViewer/App_Start/BundleConfig.cs
+++ FitnessViewer/App_Start/BundleConfig.cs
@@ -18,6 +18,9 @@ namespace FitnessViewer
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            
+
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +29,9 @@ namespace FitnessViewer
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/underscore").Include(
+                  "~/Scripts/underscore.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
