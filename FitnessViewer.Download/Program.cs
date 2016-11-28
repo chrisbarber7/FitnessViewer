c8a9f9a9a3ea33ef21fd591f8b01f9adbc5fed01
chrisbarber7 <chris@chrisbarber.co.uk>
2016-11-28 09:45:49 +0000

    Remove testing code

--- FitnessViewer.Download/Program.cs
+++ FitnessViewer.Download/Program.cs
@@ -24,42 +24,7 @@ namespace FitnessViewer.Download
 
             Infrastructure.Interfaces.IUnitOfWork _unitOfWork = new Infrastructure.Data.UnitOfWork();
 
-            var userId = "e0113fcc-7546-4c88-872f-c27e196c4d5c";
-            //       var userId2 = "b2b41546-7248-4fcc-bf6a-694672c7d0cc";
-
-
-            var metrics = _unitOfWork.CRUDRepository.GetByUserId<Metric>(userId)
-                .Where(x=>x.MetricType == MetricType.Weight).ToList();
-
-
-
-
-    var a =         _unitOfWork.CRUDRepository.GetByKey<Activity>(4395245L, o=>o.ActivityType);
-
-            Metric m = Metric.CreateMetric(userId, MetricType.Weight, DateTime.Now, 1.23M, false);
-
-            _unitOfWork.CRUDRepository.Add<Metric>(m);
-            _unitOfWork.Complete();
-
-  //          FitbitHelper h = new FitbitHelper(_unitOfWork, userId);
-
       
-           //  h.Download(false);
-       //     h.Download(true);
-
-          //  StravaUpdate upd = new StravaUpdate(_unitOfWork, userId2);
-          //  upd.UpdatePrivateFlag(774380153);
-
-   //         ActivityWeight w1 = new ActivityWeight(userId);
-   //         w1.UpdateActivityWeight();
-
-
-
-            ActivityWeight w = new ActivityWeight(userId, 771368208);
-            decimal? we = w.GetActivityWeight();
-
-
-
             while (true)
             {
                 var jobs = _unitOfWork.Queue.GetQueue(20);
