d108657c249fcea011ee998f3c318762dd97e034
chrisbarber7 <chris@chrisbarber.co.uk>
2016-12-06 11:06:57 +0000

    Making a start on a user setting page.  Add FTP details.

--- FitnessViewer/Controllers/SettingsController.cs
+++ FitnessViewer/Controllers/SettingsController.cs
@@ -1,7 +1,10 @@
 ﻿using FitnessViewer.Infrastructure.Interfaces;
+using FitnessViewer.Infrastructure.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,11 +23,79 @@ namespace FitnessViewer.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [Authorize]
+        public ActionResult FTPDelete(long? id)
+        {
+            if (id == null)
+                return Json(new { success = false, responseText = "Error Deleting record.", id = id }, JsonRequestBehavior.AllowGet);
+
+            Zone toDelete = _unitOfWork.CRUDRepository.GetByKey<Zone>((int)id.Value);
+
+            // check zone exists and it belongs to the correct user
+            if ((toDelete == null) || (toDelete.UserId != User.Identity.GetUserId()))
+                return Json(new { success = false, responseText = "Error Deleting record.", id = id }, JsonRequestBehavior.AllowGet);
+
+            _unitOfWork.CRUDRepository.Delete<Zone>(toDelete);
+            _unitOfWork.Complete();
+
+            return Json(new { success = true, responseText = "Record Deleted.", id = id }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult FTPUpdate(Zone viewModel)
+        {
+            // userid not provided so need to populate and clear any model errors.
+            viewModel.UserId = User.Identity.GetUserId();
+                ModelState["UserId"].Errors.Clear();
+
+            if (!ModelState.IsValid)
+                return Json(new { success = false, responseText = "Error.", id = viewModel.Id }, JsonRequestBehavior.AllowGet);
+
+            if (viewModel.Id == int.MaxValue)
+            {
+                _unitOfWork.CRUDRepository.Add<Zone>(viewModel);
+                _unitOfWork.Complete();
+                return Json(new { success = true, responseText = "Record Added Sucessfully.", id=viewModel.Id }, JsonRequestBehavior.AllowGet);
+
+            }
+            else
+            {
+                _unitOfWork.CRUDRepository.Update<Zone>(viewModel);
+                _unitOfWork.Complete();
+                return Json(new { success = true, responseText = "Record Updated Sucessfully.", id = viewModel.Id }, JsonRequestBehavior.AllowGet);
+            }
+        }
 
         public ActionResult Power()
         {
-            return View();
+  
+            return View(GetPowerZoneInformation());
         }
+
+        [HttpGet]
+        public ActionResult GetZoneGridInformation()
+        {
+            return PartialView("_zoneGrid", GetPowerZoneInformation());
+        }
+
+        private List<Zone> GetPowerZoneInformation()
+        {
+            var power = _unitOfWork.Settings.GetUserZones(this.User.Identity.GetUserId(), Infrastructure.enums.ZoneType.BikePower).ToList();
+
+            // add an extra row which will be hidden on the grid but enabled/used for editing.
+            power.Add(new Infrastructure.Models.Zone()
+            {
+                Id = int.MaxValue,
+                ZoneType = Infrastructure.enums.ZoneType.BikePower,
+                StartDate = DateTime.Now,
+                Value = 0
+            });
+
+            return power.OrderBy(a => a.StartDate).ToList();
+        }
+
 
         public ActionResult Pace()
         {
---
+++ FitnessViewer/Scripts/app/Controllers/settingsController.js
@@ -0,0 +1,99 @@
+﻿var settingsController = function () {
+    var init = function () {
+        var dotNetMaxInt = 2147483647;
+
+        $("#add").click(function (e) {
+            var row = $("tr[data-id='" + dotNetMaxInt + "']");
+            row.removeClass("Hidden");
+            $("#add").addClass("Hidden");
+            editRow(dotNetMaxInt);
+            e.preventDefault();
+        });
+
+        // edit
+        $(".glyphicon-pencil").click(function (e) {
+            var id = $(this).closest('tr').attr("data-id");         
+            editRow(id);
+        });
+
+        // ok
+        $(".glyphicon-ok").click(function (e) {
+            var id = $(this).closest('tr').attr("data-id");
+           
+            var model = { Id: id, Value: $("#valueEdit_" + id).val(), ZoneType: 1, StartDate: $("#dateEdit_" + id).val() };
+            $.post("FTPUpdate", model, function (data) {
+                if (data.success)
+                {
+                    refreshGrid();
+                }
+                else
+                {
+                    showError(data.responseText);
+                    resetIcons(id);
+                }
+            });
+        });
+
+        // delete
+        $(".glyphicon-trash").click(function (e) {
+            var id = $(this).closest('tr').attr("data-id");
+
+            //var model = { Id: id, Value: $("#valueEdit_" + id).val(), ZoneType: 1, StartDate: $("#dateEdit_" + id).val() };
+            $.post("FTPDelete/"+id, id, function (data) {
+                if (data.success) {
+                    refreshGrid();
+                }
+                else {
+                    showError(data.responseText);
+                }
+            });
+
+        });
+
+        // cancel edit
+        $(".glyphicon-remove").click(function (e) {
+            var id = $(this).closest('tr').attr("data-id"); 
+            resetIcons(id);
+        });
+
+        function editRow(id) {
+            toggleHidden(id);
+
+        };
+
+        function resetIcons(id) {
+            var row = $("tr[data-id='" + id + "']");
+            if (id == dotNetMaxInt) {
+                row.addClass("Hidden");
+                $("#add").removeClass("Hidden");
+            }
+            toggleHidden(id);
+        }
+
+        function toggleHidden(id) {
+            var row = $("tr[data-id='" + id + "']");
+            $(row).find("#save").toggleClass("Hidden");
+            $(row).find("#cancel").toggleClass("Hidden");
+            $(row).find("#valueEdit_" + id).toggleClass("Hidden");
+            $(row).find("#dateEdit_" + id).toggleClass("Hidden");
+            $(row).find("#valueView").toggleClass("Hidden");
+            $(row).find("#dateView").toggleClass("Hidden");
+            $(row).find("#delete").toggleClass("Hidden");
+            $(row).find("#edit").toggleClass("Hidden");
+        }
+
+        function showError(message) {
+            $("#error").removeClass("Hidden");
+            $("#errorMessage").text(message);
+        };
+        
+        function refreshGrid() {
+            //$("#zoneGrid").load("/settings/GetZoneGridInformation");
+            //settingsController.init();
+            location.reload();
+        };
+    };
+    return {
+        init: init
+    };
+}();
