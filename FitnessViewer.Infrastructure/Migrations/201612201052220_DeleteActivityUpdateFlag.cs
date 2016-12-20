c62cbe86966e3c31f1062b94c82c840067463055
chrisbarber7 <chris@chrisbarber.co.uk>
2016-12-20 10:58:31 +0000

    Fix to un-set details downloaded flag when removing activity

---
+++ FitnessViewer.Infrastructure/Migrations/201612201052220_DeleteActivityUpdateFlag.cs
@@ -0,0 +1,45 @@
+namespace FitnessViewer.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteActivityUpdateFlag : DbMigration
+    {
+        public override void Up()
+        {
+            AlterStoredProcedure(
+                 "dbo.ActivityDeleteDetails",
+                 p => new { ActivityId = p.Long() },
+                 body:
+                  @"BEGIN TRANSACTION
+                BEGIN TRY
+
+
+                DELETE FROM[dbo].[UserNotifications]
+                    where NotificationId in (Select id from[dbo].[Notifications]
+                                            where activityId = @ActivityId)
+
+                DELETE FROM[dbo].[Notifications] where activityId = @ActivityId
+                DELETE FROM[dbo].[ActivityPeakDetails] WHERE ActivityId = @ActivityId
+                DELETE FROM[dbo].[ActivityPeaks] WHERE ActivityId = @ActivityId
+                DELETE FROM[dbo].[BestEfforts] WHERE ActivityId = @ActivityId
+                DELETE FROM[dbo].[Laps] WHERE ActivityId = @ActivityId
+                DELETE FROM[dbo].[Streams] WHERE ActivityId = @ActivityId
+                UPDATE Activities set DetailsDownloaded = 0 where id = @ActivityId
+
+                COMMIT
+                END TRY
+                BEGIN CATCH
+                ROLLBACK TRAN
+                RAISERROR('There was an error whilst deleteing activity.  The Transaction has been rolled back', 5, 1)
+                END CATCH");
+
+
+
+    }
+        
+        public override void Down()
+        {
+        }
+    }
+}
