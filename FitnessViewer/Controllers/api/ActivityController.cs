dbf85e7cf3e79e9161d1e23985a40ed84215a865
chrisbarber7 <chris@chrisbarber.co.uk>
2017-01-08 16:13:11 +0000

    Modify how values added to chart

--- FitnessViewer/Controllers/api/ActivityController.cs
+++ FitnessViewer/Controllers/api/ActivityController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Web.Http;
-using Microsoft.AspNet.Identity;
-using FitnessViewer.Infrastructure.Models.Dto;
 using FitnessViewer.Infrastructure.Models.Collections;
 using FitnessViewer.Infrastructure.Interfaces;
-using FitnessViewer.Infrastructure.enums;
-using FitnessViewer.Infrastructure.Helpers.Conversions;
-using static FitnessViewer.Infrastructure.Helpers.DateHelpers;
+using System.Linq;
+using FitnessViewer.Infrastructure.Helpers;
 
 namespace FitnessViewer.Controllers.api
 {
@@ -49,19 +45,9 @@ namespace FitnessViewer.Controllers.api
         public IHttpActionResult GetPowerCurve(string id)
         {
             var powerCurve = ActivityPeakDetails.LoadForActivity(Convert.ToInt64(id)).GetPowerCurve();
-
-            List<string> duration = new List<string>();
-            List<string> watts = new List<string>();
-
-            foreach (PowerCurveDto p in powerCurve)
-            {
-                duration.Add(p.Duration.ToString());
-                watts.Add(p.Watts.ToString());
-            }
-
-            var chart = new { 
-            Duration=duration,
-            Watts = watts
+            var chart = new {
+                Duration = powerCurve.Select(a => DisplayLabel.ShortStreamDurationForDisplay(a.Duration)).ToArray(),
+                Watts = powerCurve.Select(a=>a.Watts.ToString()).ToArray()
             };
             return Ok(chart);
         }
